0af02e3f169d64b69d3b630a8e0a4d640a64dfef
dackular <dackular@hotmail.com>
2021-01-05 14:11:24 +0700

    first push js

---
+++ Project001/app.js
@@ -0,0 +1,60 @@
+        // console.log("Hello DACKular")
+        // console.log("Hello DACKular")
+        // console.log("Hello DACKular")
+        // //alert("DACKular")
+
+        // var name = "DACK"
+        // console.log(name)
+        // console.log(name)
+        // name = "TOD"
+        // console.log(name)
+        // name = "Jame"
+        // console.log(name)
+        
+        // var FirstName = "Chartiporn"
+        // var LastName = "Charnsamai"
+        // console.log(FirstName)
+        // console.log(LastName)
+
+        // //alert(FirstName + " " + LastName)
+        // console.log(FirstName + " " + LastName)
+
+        // var age = 2+2*2
+        // //alert(typeof age)
+        // console.log(typeof age)
+        // //alert(typeof name)
+        // console.log(typeof name)
+        // console.log(age)
+
+        // var num = (4+2*2/2*2+4+1-5+19)+91
+        // console.log(num)
+        // //alert(num)
+        // console.log(num)
+
+        // var x = 20
+
+        // {
+        //     var x = 200
+        //     let y = 15
+        //     console.log(y)
+        // }
+        // console.log(x)
+
+        // let dataName = prompt("กรอกข้อมูลมาหน่อยซิ")
+        // console.log("Hello " + dataName)
+
+// ================================
+
+let lotto_number = prompt("กรุณากรอกตัวเลขที่ต้องการซื้อ")
+document.getElementById("buyNum").innerHTML = lotto_number
+document.getElementById("result").innerHTML = Math.floor(Math.random() * 1000)
+console.log(document.getElementById("buyNum").innerHTML)
+console.log(document.getElementById("result").innerHTML)
+
+// =============================
+
+
+// console.log(1 == 1)
+// console.log(2 == 2)
+// console.log(3 == 3)
+// console.log(4 == "4")
