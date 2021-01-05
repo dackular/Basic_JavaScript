e6cc9f7dc8f5f35d90e6c4af60e69cbff8a608de
dackular <dackular@hotmail.com>
2021-01-05 14:14:41 +0700

    update project 002

---
+++ Project002/app.js
@@ -0,0 +1,5 @@
+let age = prompt("กรุณากรอกอายุของคุณ")
+if (age > 13) {
+    // console.log("Your age is OK")
+    document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/O1gtH6GCOR4\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+}
