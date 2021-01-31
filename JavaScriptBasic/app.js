8329cca97bf1cdcaca4f73e11806d82078542649
dackular <dackular@hotmail.com>
2021-01-31 16:22:00 +0700

    update document and comment

---
+++ JavaScriptBasic/app.js
@@ -0,0 +1,11 @@
+// แสดงข้อความในหน้าเว็ป
+document.write("<h1>DACKular.com</h1>");
+document.write("<p>JavaScript เบื้องต้น</p>")
+alert("สวัสดีครับ");
+/* แสดงข้อความใน
+แท็ป console
+ของเว็ป browser
+*/
+console.log("Hello JavaScript");
+console.error("เจอไวรัส");
+console.warn("เจอข้อผิดพลาด")
