95695722e4ad4e0054f53d26bcbd786b112531a9
dackular <dackular@hotmail.com>
2021-02-02 14:18:16 +0700

    update type conversion

---
+++ JavaScriptBasic/typeconversion.js
@@ -0,0 +1,31 @@
+let money = 500; // number (integer)
+// let price = 99.99 // number (float)
+// let price = 99.99+""; // String
+let price = 99.99.toString(); // String
+
+let message = 'DACK'
+let name = "DACKular"
+
+let checkName = false
+let checkProduct = true;
+
+// let age = parseInt("20.15");
+// let age = parseFloat("20.15");
+let age = +"20.15";
+
+// console.log(money);
+console.log(price);
+// console.log(message);
+// console.log(name);
+// console.log(checkName);
+// console.log(checkProduct);
+console.log(age);
+
+
+// console.log(typeof(money));
+console.log(typeof(price));
+// console.log(typeof(message));
+// console.log(typeof(name));
+// console.log(typeof(checkName));
+// console.log(typeof(checkProduct));
+console.log(typeof(age));
