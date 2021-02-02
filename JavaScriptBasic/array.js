d1537460d645d04037fd7ddf5c7a06b810d8d0f8
dackular <dackular@hotmail.com>
2021-02-02 14:48:41 +0700

    update array

---
+++ JavaScriptBasic/array.js
@@ -0,0 +1,35 @@
+// let number1 = 1 , number2 = 2 , number3 = 3 , number4 = 4 , number5 = 5;
+// let number1=1;
+// let number2=2;
+// let number3=3;
+// let number4=4;
+// let number5=5;
+// let number6=6;
+
+let number = Array(1,2,3,4,5,6);
+
+// console.log(number2);
+// console.log(number3);
+
+console.log(number);
+
+let number1 = Array(100,200,300,400,500,600);
+console.log(number1);
+
+// index เริ่มต้อนที่เลข 0 หมายถึงสมาชิกตัวแรก
+console.log("ตัวแรก : ",number[0]);
+console.log("ตัวที่ 3 : ",number1[2]);
+
+let number2 = Array(100,"200",300,400.99,500);
+console.log(number2);
+
+
+let color = ["แดง","ชมพู","เขียว","ขาว"];
+console.log(color);
+color[1] = "เหลือง";
+color[2] = "ม่วง";
+console.log(color);
+
+let days = ["Sunday","Monday","Tuesday"];
+console.log(days);
+
