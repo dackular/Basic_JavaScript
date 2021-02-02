c57b92526e9d5bba8c561e2e5501d2a4f48ab559
dackular <dackular@hotmail.com>
2021-02-02 14:10:16 +0700

    update data type

---
+++ JavaScriptBasic/datatype.js
@@ -0,0 +1,27 @@
+let money = 500; // number (integer)
+let price = 99.99 // number (float)
+
+let message = 'DACK'
+let name = "DACKular"
+
+let checkName = false
+let checkProduct = true;
+
+let age = '20'; // string
+
+console.log(money);
+console.log(price);
+console.log(message);
+console.log(name);
+console.log(checkName);
+console.log(checkProduct);
+console.log(age);
+
+
+console.log(typeof(money));
+console.log(typeof(price));
+console.log(typeof(message));
+console.log(typeof(name));
+console.log(typeof(checkName));
+console.log(typeof(checkProduct));
+console.log(typeof(age));
