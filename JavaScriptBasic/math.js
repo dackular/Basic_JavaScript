e34b475649987aadd9db516bd02494af924b5ccd
dackular <dackular@hotmail.com>
2021-02-02 16:48:07 +0700

    update math

---
+++ JavaScriptBasic/math.js
@@ -0,0 +1,8 @@
+let a=20, b=10;
+
+console.log("ผลบวก = ",a+b);
+console.log("ผลลบ = ",a-b);
+console.log("ผลคูณ = ",20*10);
+console.log("ผลหาร = ",20/3);
+console.log("ผลเศษ = ",20%3);
+console.log("ยกกำลัง = ",5**2);
