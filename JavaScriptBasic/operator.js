5b12aa4d93e81447e4073a0563675351be1d1e97
dackular <dackular@hotmail.com>
2021-02-03 13:33:43 +0700

    update operator

---
+++ JavaScriptBasic/operator.js
@@ -0,0 +1,12 @@
+let a = 5 , b = 10;
+
+let check = false;
+
+console.log("เท่ากันหรือไม่ = ", a == b);
+console.log("ไม่เท่ากันหรือไม่ = ",a != b);
+console.log("มากกว่าหรือไม่ = ",a > b);
+console.log("น้อยกว่าหรือไม่ = ", a < b);
+console.log("มากกว่าหรือเท่ากับ = ", a >= b);
+console.log("น้อยกว่าหรือเท่ากับ = ", a <= b);
+
+console.log(!check);
