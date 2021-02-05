33e84dea359cb96ca3422c32b07911551e1bce5d
dackular <dackular@hotmail.com>
2021-02-05 22:48:21 +0700

    update operstor sequence

--- JavaScriptBasic/operator.js
+++ JavaScriptBasic/operator.js
@@ -10,3 +10,9 @@ console.log("มากกว่าหรือเท่ากับ = ", a >= b)
 console.log("น้อยกว่าหรือเท่ากับ = ", a <= b);
 
 console.log(!check);
+
+console.log(5+8*9);
+console.log(10-4+2);
+console.log(10-(2+1));
+console.log(5*2-40/5);
+console.log(7+8/2+25);
