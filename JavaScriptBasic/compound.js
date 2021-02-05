e698deb9fa341f3a6c127e26606bf00086bd92c3
dackular <dackular@hotmail.com>
2021-02-05 22:32:49 +0700

    updatet compound assignment

---
+++ JavaScriptBasic/compound.js
@@ -0,0 +1,16 @@
+let x = 10, y = 20;
+
+// before
+console.log("Before x = ",x);
+
+
+// x = x+y; 
+x+=y; 
+
+// x-=y; => x=x-y;
+// x*=y; => x=x*y;
+// x/=y; => x=x/y;
+// x%=y; => x=x%y;
+
+console.log("After x+y : ",x);
+
