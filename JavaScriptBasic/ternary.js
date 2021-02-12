9b769a3da386ebc51dbfa415bfe20210403fc8fc
dackular <dackular@hotmail.com>
2021-02-12 12:22:59 +0700

    update ternary

---
+++ JavaScriptBasic/ternary.js
@@ -0,0 +1,13 @@
+let score = 80; //100 คะแนน
+
+// let pass;
+
+// if(score>50){
+//     pass = "ผ่านเกณฑ์";
+// }else{
+//     pass = "ไม่ผ่านเกณฑ์"
+// }
+
+let pass = score>50 ? "ผ่านเกณฑ์" : "ไ่ม่ผ่านเกณฑ์";
+
+console.log(pass)
