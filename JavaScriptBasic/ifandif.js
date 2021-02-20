2ffc6df3dd3fe8518e0217d3a05a834cfa8ecb50
dackular <dackular@hotmail.com>
2021-02-20 08:55:13 +0700

    update if and if

---
+++ JavaScriptBasic/ifandif.js
@@ -0,0 +1,15 @@
+let age = 90;
+
+if(age<=15){
+    if(age==15){
+        console.log("ม.3")
+    }else if(age==14){
+        console.log("ม.2")
+    }else if(age==13){
+        console.log("ม.1")
+    }else{
+        console.log("ประถมศึกษา")
+    }
+}else{
+    console.log("ม.ปลาย / ปริญญา")
+}
